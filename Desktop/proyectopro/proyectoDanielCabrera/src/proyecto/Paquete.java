d02a7afc332994edf06b3209223df43712dc887b
Daniel <danielcabrera@ieslasgalletas.net>
2021-03-21 20:21:23 +0000

    Prueba 2

--- Desktop/proyectopro/proyectoDanielCabrera/src/proyecto/Paquete.java
+++ Desktop/proyectopro/proyectoDanielCabrera/src/proyecto/Paquete.java
@@ -1,5 +1,36 @@
 package proyecto;
 
 public class Paquete {
+    private int numeroPaquete;
+    private String dni;
+    private double peso;
+    private int prioridad; //0=normal 1 = alta 2=expres
+
+    public Paquete(int numeroReferencia, String dni, double peso, int prioridad) {
+        this.numeroPaquete = numeroPaquete;
+        this.dni = dni;
+        this.peso = peso;
+        this.prioridad = prioridad;
+    }
+
+    public int getNumeroPaquete() {
+        return numeroPaquete;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "Paquete{" + "numeroPaquete=" + numeroPaquete + ", dni=" + dni + ", peso=" + peso + ", prioridad=" + prioridad + '}';
+    }
     
 }
