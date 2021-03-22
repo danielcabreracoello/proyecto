864564da3c93055e768f4bbbe61a8dbce67f51c4
Daniel <danielcabrera@ieslasgalletas.net>
2021-03-22 19:37:14 +0000

    Prueba 3

--- Desktop/proyectopro/proyectoDanielCabrera/src/proyecto/Sucursal.java
+++ Desktop/proyectopro/proyectoDanielCabrera/src/proyecto/Sucursal.java
@@ -1,5 +1,42 @@
 package proyecto;
 
 public class Sucursal {
-    
+    private int numeroSucursal;
+    private String direccion;
+    private String ciudad;
+
+    public Sucursal(int numeroSucursal, String dirrecion, String ciudad) {
+        this.numeroSucursal = numeroSucursal;
+        this.direccion = dirrecion;
+        this.ciudad = ciudad;
+    }
+
+    public int getNumeroSucursal() {
+        return numeroSucursal;
+    }
+
+    public String getDirrecion() {
+        return direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String mostrarDatosSucursal() {
+        return "\nNumero sucursal: " + numeroSucursal
+                + "\nDirrecion: " + direccion
+                + "\nCiudad: " + ciudad;
+    }
+
+    public double calcular_precio(Paquete t) {
+        double precio;
+        precio = t.getPeso();
+        if (t.getPrioridad() == 1) {
+            precio = precio + 10;
+        } else if (t.getPrioridad() == 2) {
+            precio = precio + 20;
+        }
+        return precio;
+    }
 }
